54df2f20f2d95ac68f2031a2c3d490452239d965
Ishikajain02 <ishijain02@gmail.com>
2023-04-06 14:32:30 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>My favourite Food Items</h1>
+       <ul>
+        <li>Maggi</li>
+        <li>Fruits</li>
+       <li>Vegetables</li>
+       </ul>
+      
       </header>
     </div>
   );

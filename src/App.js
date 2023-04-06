e4d4cf4c0e6ecd059ec4ca6d154a7c6b05a4e321
Ishikajain02 <ishijain02@gmail.com>
2023-04-07 01:00:00 +0530

    practicing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const num = 9;
+var year = new Date().getFullYear();
+var time = new Date().getHours();
+var customStyle = {
+  color : ""
+}
+var greeting;
+if(time<12){
+ greeting ="Good Morning";
+ customStyle.color = "green";
+}
+else if(time>12 && time<18){
+  greeting = "Good Afternoon";
+  customStyle.color = "blue";
+}
+else{
+  greeting = "Good Evening";
+  customStyle.color = "pink";
+}
 function App() {
   return (
     <div className="App">
@@ -13,6 +32,13 @@ function App() {
        </ul>
       
       </header>
+      
+      
+      <div>
+        <h3>Your lucky number is {num}</h3>
+        <h2>Created by me in {year}</h2>
+      </div>
+      <h1 style ={customStyle}>{greeting}</h1>
     </div>
   );
 }

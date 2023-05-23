00ed28570f95ecdd57284adee3a280679c5cc452
Ishikajain02 <ishijain02@gmail.com>
2023-05-23 13:30:32 +0530

    making general changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import functionn from './functionn';
-import jaaava from './jaaava';
+
+
 
 
 
 function App() {
   return (
-    <functionn />
+    <div>App</div>
     
   );
 }

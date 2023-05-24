6251a76d1053f0c31b849fed18143a1cbceaae21
Ishikajain02 <ishijain02@gmail.com>
2023-05-24 15:13:03 +0530

    revision

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-
-import List from "./src/List";
-
+import { ReactDOM } from "react";
+import React, { useState } from "react";
 
 
 function App() {
+  const[newValue, Setval] = useState("");
+  const[too,setto] = useState([]);
+  function changee(event){
+    const nevv= event.target.value;
+    Setval(nevv);
+  }
+  function funcc(){
+    
+      setto( too => {
+        return [...too , newValue]
+      });
+  }
   return (
     <div>
-    <div>My to do list</div>
-    
+    <div>
+      <input onChange = {changee}type = "text " placeholder ="enter the task" value ={newValue}/>
+      <button onClick={funcc}>+</button>
     </div>
-  );
-}
+    <div>
+    <ol>
+      {too.map(toos  =>  <li>
+        {too}
+      </li>)}
+    </ol>
+    </div>
+    </div>
+    );
+ }
+ 
+
+  
+ 
+
+ 
+
 
 
 
